5cd57433fcfca4b17678e61f283b058ae08ab557
Prof.JMDavis <Prof.JMDavis@gmail.com>
2022-09-29 11:13:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,20 @@
       
     function hideMenu() {
         document.getElementById("contextMenu")
-                .style.display = "none"
+                contextMenu.style.display = "none"
     }
   
-    function rightClick(e) {
-        e.preventDefault();
-  
+    function rightClick(event) {
+        event.preventDefault();
+	var contextMenu = document.getElementById("contextMenu");
         if (document.getElementById("contextMenu")
                 .style.display == "block")
             hideMenu();
         else{
-            var menu = document.getElementById("contextMenu")
-  
-            menu.style.display = 'block';
-            menu.style.left = e.pageX + "px";
-            menu.style.top = e.pageY + "px";
+            var contextMenu = document.getElementById("contextMenu");
+            contextMenu.style.display = 'block';
+            contextMenu.style.left = e.pageX + "px";
+            contextMenu.style.top = e.pageY + "px";
         }
     }
 	function MarkSquare(){
