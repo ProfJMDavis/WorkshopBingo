aed20b843e4b04204758a3c906f5e62b8317d59d
Prof.JMDavis <Prof.JMDavis@gmail.com>
2022-09-29 11:27:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,9 @@
     function rightClick(event) {
         event.preventDefault();
 	var contextMenu = document.getElementById("contextMenu");
-        if (document.getElementById("contextMenu") {
+        if (document.getElementById("contextMenu")
             contextMenu.style.display = 'block';
             hideMenu();
-    }
         else{
             var contextMenu = document.getElementById("contextMenu");
             contextMenu.style.display = 'block';
@@ -25,75 +24,75 @@
 	if(document.getElementById(varName)){
 		switch(varName) {
 		case "G1":
-				document.getElementById("G1").src = "Images/Green_Torch.png";
+				document.getElementById("G1").src = "Green_Torch.png";
 				break;
 		case "B1":
-				document.getElementById("B1").src = "Images/Blue_Torch.png";
+				document.getElementById("B1").src = "Blue_Torch.png";
 				break;
    		case "R1": 
-				document.getElementById("R1").src = "Images/Red_Torch.png";
+				document.getElementById("R1").src = "Red_Torch.png";
 				break;
 		case "P1": 
-				document.getElementById("P1").src = "Images/Purple_Torch.png";
+				document.getElementById("P1").src = "Purple_Torch.png";
 				break;
 		case "G12":
-				document.getElementById("G12").src = "Images/Green_Torch.png";
+				document.getElementById("G12").src = "Green_Torch.png";
 				break;
 		case "B2":
-				document.getElementById("B2").src = "Images/Blue_Torch.png";
+				document.getElementById("B2").src = "Blue_Torch.png";
 				break;
    		case "R2": 
-				document.getElementById("R2").src = "Images/Red_Torch.png";
+				document.getElementById("R2").src = "Red_Torch.png";
 				break;
 		case "P2": 
-				document.getElementById("P2").src = "Images/Purple_Torch.png";
+				document.getElementById("P2").src = "Purple_Torch.png";
 				break;
 		case "G2":
-				document.getElementById("G2").src = "Images/Green_Torch.png";
+				document.getElementById("G2").src = "Green_Torch.png";
 				break;
 		case "B22":
-				document.getElementById("B22").src = "Images/Blue_Torch.png";
+				document.getElementById("B22").src = "Blue_Torch.png";
 				break;
    		case "R3": 
-				document.getElementById("R3").src = "Images/Red_Torch.png";
+				document.getElementById("R3").src = "Red_Torch.png";
 				break;
 		case "P3": 
-				document.getElementById("P3").src = "Images/Purple_Torch.png";
+				document.getElementById("P3").src = "Purple_Torch.png";
 				break;
 		case "G3":
 				document.getElementById("G3").src = "Images/Green_Torch.png";
 				break;
 		case "B3":
-				document.getElementById("B3").src = "Images/Blue_Torch.png";
+				document.getElementById("B3").src = "Blue_Torch.png";
 				break;
    		case "R4": 
-				document.getElementById("R4").src = "Images/Red_Torch.png";
+				document.getElementById("R4").src = "Red_Torch.png";
 				break;
 		case "P4": 
-				document.getElementById("P4").src = "Images/Purple_Torch.png";
+				document.getElementById("P4").src = "Purple_Torch.png";
 		case "G4":
-				document.getElementById("G4").src = "Images/Green_Torch.png";
+				document.getElementById("G4").src = "Green_Torch.png";
 				break;
 		case "B4":
-				document.getElementById("B4").src = "Images/Blue_Torch.png";
+				document.getElementById("B4").src = "Blue_Torch.png";
 				break;
    		case "R42": 
-				document.getElementById("R42").src = "Images/Red_Torch.png";
+				document.getElementById("R42").src = "Red_Torch.png";
 				break;
 		case "P5": 
-				document.getElementById("P5").src = "Images/Purple_Torch.png";
+				document.getElementById("P5").src = "Purple_Torch.png";
 				break;
 		case "G5":
-				document.getElementById("G5").src = "Images/Green_Torch.png";
+				document.getElementById("G5").src = "Green_Torch.png";
 				break;
 		case "P52": 
-				document.getElementById("P52").src = "Images/Purple_Torch.png";
+				document.getElementById("P52").src = "Purple_Torch.png";
 				break;
 		case "B5":
-				document.getElementById("B5").src = "Images/Blue_Torch.png";
+				document.getElementById("B5").src = "Blue_Torch.png";
 				break;
 		case "R5": 
-				document.getElementById("R5").src = "Images/Red_Torch.png";
+				document.getElementById("R5").src = "Red_Torch.png";
 				break;
         alert('Square Marked');
        	break;
@@ -109,75 +108,75 @@
 			if(document.getElementById(varName)){
 				switch(varName) {
 					case "G1":
-						document.getElementById("G1").src = "Images/Green_Square.png";
+						document.getElementById("G1").src = "Green_Square.png";
 						break;
 					case "B1":
-						document.getElementById("B1").src = "Images/Blue_Square.png";
+						document.getElementById("B1").src = "Blue_Square.png";
 						break;
 					case "R1": 
-						document.getElementById("R1").src = "Images/Red_Square.png";
+						document.getElementById("R1").src = "Red_Square.png";
 						break;
 					case "P1":
-						document.getElementById("P1").src = "Images/Purple_Square.png";
+						document.getElementById("P1").src = "Purple_Square.png";
 						break;
 					case "G12":
-						document.getElementById("G12").src = "Images/Green_Square.png";
+						document.getElementById("G12").src = "Green_Square.png";
 						break;
 					case "B2":
-						document.getElementById("B2").src = "Images/Blue_Square.png";
+						document.getElementById("B2").src = "Blue_Square.png";
 						break;
 					case "R2": 
-						document.getElementById("R2").src = "Images/Red_Square.png";
+						document.getElementById("R2").src = "Red_Square.png";
 						break;
 					case "P2": 
-						document.getElementById("P2").src = "Images/Purple_Square.png";
+						document.getElementById("P2").src = "Purple_Square.png";
 						break;
 					case "G2":
-						document.getElementById("G2").src = "Images/Green_Square.png";
+						document.getElementById("G2").src = "Green_Square.png";
 						break;
 					case "B22":
-						document.getElementById("B22").src = "Images/Blue_Square.png";
+						document.getElementById("B22").src = "Blue_Square.png";
 						break;
 					case "R3": 
-						document.getElementById("R3").src = "Images/Red_Square.png";
+						document.getElementById("R3").src = "Red_Square.png";
 						break;
 					case "P3": 
-						document.getElementById("P3").src = "Images/Purple_Square.png";
+						document.getElementById("P3").src = "Purple_Square.png";
 					case "G3":
-						document.getElementById("G3").src = "Images/Green_Square.png";
+						document.getElementById("G3").src = "Green_Square.png";
 						break;
 					case "B3":
-						document.getElementById("B3").src = "Images/Blue_Square.png";
+						document.getElementById("B3").src = "Blue_Square.png";
 						break;
    					case "R4": 
-						document.getElementById("R4").src = "Images/Red_Square.png";
+						document.getElementById("R4").src = "Red_Square.png";
 						break;
 					case "P4": 
-						document.getElementById("P4").src = "Images/Purple_Square.png";
+						document.getElementById("P4").src = "Purple_Square.png";
 						break;
 					case "G4":
-						document.getElementById("G4").src = "Images/Green_Square.png";
+						document.getElementById("G4").src = "Green_Square.png";
 						break;
 					case "B4":
-						document.getElementById("B4").src = "Images/Blue_Square.png";
+						document.getElementById("B4").src = "Blue_Square.png";
 						break;
    					case "R42": 
-						document.getElementById("R42").src = "Images/Red_Square.png";
+						document.getElementById("R42").src = "Red_Square.png";
 						break;
 					case "P5": 
-						document.getElementById("P5").src = "Images/Purple_Square.png";
+						document.getElementById("P5").src = "Purple_Square.png";
 						break;
 					case "G5":
-						document.getElementById("G5").src = "Images/Green_Square.png";
+						document.getElementById("G5").src = "Green_Square.png";
 						break;
 					case "P52": 
-						document.getElementById("P52").src = "Images/Purple_Square.png";
+						document.getElementById("P52").src = "Purple_Square.png";
 						break;
 					case "B5":
-						document.getElementById("B5").src = "Images/Blue_Square.png";
+						document.getElementById("B5").src = "Blue_Square.png";
 						break;
 					case "R5": 
-						document.getElementById("R5").src = "Images/Red_Square.png";
+						document.getElementById("R5").src = "Red_Square.png";
 						break;
         				alert('Square Unmarked');
        					break;
@@ -188,6 +187,7 @@
 					}
 				}
 			}
+        }
         function VoucherSourcetoPrint(source) {
             return "<html><head><script>function step1(){\n" +
                     "setTimeout('step2()', 10);}\n" +
@@ -195,12 +195,16 @@
                     "</scri" + "pt></head><body onload='step1()'>\n" +
                     "<img src='" + source + "' /></body></html>";
         }
-        function VoucherPrint(source) {
+        function CardPrint() {
+           var divContents = document.getElementById("photo").innerHTML;
             Pagelink = "about:blank";
             var pwa = window.open(Pagelink, "_new");
-            pwa.document.open();
-            pwa.document.write(VoucherSourcetoPrint(source));
+            pwa.document.write('<html>');
+            pwa.document.write('<body > <h1>Div contents are <br>');
+            pwa.document.write(divContents);
+            pwa.document.write('</body></html>');
             pwa.document.close();
+            pwa.print();
         }
 	    function ChangeTextIL(){
 		document.getElementById("divElement").innerHTML="Information Literacy: 1) Finding Scholarly Resources Effectively, 2) Fake News & Evaluating Information, 3) Plagiarism. Right-click to add Code from a completed workshop.";
