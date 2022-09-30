822ed6de04ca1d7e37710d1032277c8ce904c30b
Prof.JMDavis <Prof.JMDavis@gmail.com>
2022-09-30 00:37:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,90 +8,92 @@
   
     function rightClick(event) {
         event.preventDefault();
-  	    var contextmenu = document.getElementById("contextMenu");
-	    if (document.getElementById("contextMenu")
-            contextmenu.style.display = 'block';
+  
+        if (document.getElementById("contextMenu")
+                .style.display == "block")
             hideMenu();
         else{
-            contextmenu.style.display = 'block';
-            contextmenu.style.left = event.pageX + "px";
-            contextmenu.style.top = event.pageY + "px";
+            var menu = document.getElementById("contextMenu")
+  
+            menu.style.display = 'block';
+            menu.style.left = e.pageX + "px";
+            menu.style.top = e.pageY + "px";
         }
     }
 	function MarkSquare(){
-	var varName = document.getElementById(string);
+	let string = varName;
 	if(document.getElementById(varName)){
 		switch(varName) {
 		case "G1":
-				document.getElementById("G1").src = "Green_Torch.png";
+				document.getElementById("G1").src = "Images/Green_Torch.png";
 				break;
 		case "B1":
-				document.getElementById("B1").src = "Blue_Torch.png";
+				document.getElementById("B1").src = "Images/Blue_Torch.png";
 				break;
    		case "R1": 
-				document.getElementById("R1").src = "Red_Torch.png";
+				document.getElementById("R1").src = "Images/Red_Torch.png";
 				break;
 		case "P1": 
-				document.getElementById("P1").src = "Purple_Torch.png";
+				document.getElementById("P1").src = "Images/Purple_Torch.png";
 				break;
 		case "G12":
-				document.getElementById("G12").src = "Green_Torch.png";
+				document.getElementById("G12").src = "Images/Green_Torch.png";
 				break;
 		case "B2":
-				document.getElementById("B2").src = "Blue_Torch.png";
+				document.getElementById("B2").src = "Images/Blue_Torch.png";
 				break;
    		case "R2": 
-				document.getElementById("R2").src = "Red_Torch.png";
+				document.getElementById("R2").src = "Images/Red_Torch.png";
 				break;
 		case "P2": 
-				document.getElementById("P2").src = "Purple_Torch.png";
+				document.getElementById("P2").src = "Images/Purple_Torch.png";
 				break;
 		case "G2":
-				document.getElementById("G2").src = "Green_Torch.png";
+				document.getElementById("G2").src = "Images/Green_Torch.png";
 				break;
 		case "B22":
-				document.getElementById("B22").src = "Blue_Torch.png";
+				document.getElementById("B22").src = "Images/Blue_Torch.png";
 				break;
    		case "R3": 
-				document.getElementById("R3").src = "Red_Torch.png";
+				document.getElementById("R3").src = "Images/Red_Torch.png";
 				break;
 		case "P3": 
-				document.getElementById("P3").src = "Purple_Torch.png";
+				document.getElementById("P3").src = "Images/Purple_Torch.png";
 				break;
 		case "G3":
 				document.getElementById("G3").src = "Images/Green_Torch.png";
 				break;
 		case "B3":
-				document.getElementById("B3").src = "Blue_Torch.png";
+				document.getElementById("B3").src = "Images/Blue_Torch.png";
 				break;
    		case "R4": 
-				document.getElementById("R4").src = "Red_Torch.png";
+				document.getElementById("R4").src = "Images/Red_Torch.png";
 				break;
 		case "P4": 
-				document.getElementById("P4").src = "Purple_Torch.png";
+				document.getElementById("P4").src = "Images/Purple_Torch.png";
 		case "G4":
-				document.getElementById("G4").src = "Green_Torch.png";
+				document.getElementById("G4").src = "Images/Green_Torch.png";
 				break;
 		case "B4":
-				document.getElementById("B4").src = "Blue_Torch.png";
+				document.getElementById("B4").src = "Images/Blue_Torch.png";
 				break;
    		case "R42": 
-				document.getElementById("R42").src = "Red_Torch.png";
+				document.getElementById("R42").src = "Images/Red_Torch.png";
 				break;
 		case "P5": 
-				document.getElementById("P5").src = "Purple_Torch.png";
+				document.getElementById("P5").src = "Images/Purple_Torch.png";
 				break;
 		case "G5":
-				document.getElementById("G5").src = "Green_Torch.png";
+				document.getElementById("G5").src = "Images/Green_Torch.png";
 				break;
 		case "P52": 
-				document.getElementById("P52").src = "Purple_Torch.png";
+				document.getElementById("P52").src = "Images/Purple_Torch.png";
 				break;
 		case "B5":
-				document.getElementById("B5").src = "Blue_Torch.png";
+				document.getElementById("B5").src = "Images/Blue_Torch.png";
 				break;
 		case "R5": 
-				document.getElementById("R5").src = "Red_Torch.png";
+				document.getElementById("R5").src = "Images/Red_Torch.png";
 				break;
         alert('Square Marked');
        	break;
@@ -103,79 +105,79 @@
 		}
 	}
 		function UnMarkSquare(){
-			var varName = document.getElementById(string);
+			let string = varName;
 			if(document.getElementById(varName)){
 				switch(varName) {
 					case "G1":
-						document.getElementById("G1").src = "Green_Square.png";
+						document.getElementById("G1").src = "Images/Green_Square.png";
 						break;
 					case "B1":
-						document.getElementById("B1").src = "Blue_Square.png";
+						document.getElementById("B1").src = "Images/Blue_Square.png";
 						break;
 					case "R1": 
-						document.getElementById("R1").src = "Red_Square.png";
+						document.getElementById("R1").src = "Images/Red_Square.png";
 						break;
 					case "P1":
-						document.getElementById("P1").src = "Purple_Square.png";
+						document.getElementById("P1").src = "Images/Purple_Square.png";
 						break;
 					case "G12":
-						document.getElementById("G12").src = "Green_Square.png";
+						document.getElementById("G12").src = "Images/Green_Square.png";
 						break;
 					case "B2":
-						document.getElementById("B2").src = "Blue_Square.png";
+						document.getElementById("B2").src = "Images/Blue_Square.png";
 						break;
 					case "R2": 
-						document.getElementById("R2").src = "Red_Square.png";
+						document.getElementById("R2").src = "Images/Red_Square.png";
 						break;
 					case "P2": 
-						document.getElementById("P2").src = "Purple_Square.png";
+						document.getElementById("P2").src = "Images/Purple_Square.png";
 						break;
 					case "G2":
-						document.getElementById("G2").src = "Green_Square.png";
+						document.getElementById("G2").src = "Images/Green_Square.png";
 						break;
 					case "B22":
-						document.getElementById("B22").src = "Blue_Square.png";
+						document.getElementById("B22").src = "Images/Blue_Square.png";
 						break;
 					case "R3": 
-						document.getElementById("R3").src = "Red_Square.png";
+						document.getElementById("R3").src = "Images/Red_Square.png";
 						break;
 					case "P3": 
-						document.getElementById("P3").src = "Purple_Square.png";
+						document.getElementById("P3").src = "Images/Purple_Square.png";
 					case "G3":
-						document.getElementById("G3").src = "Green_Square.png";
+						document.getElementById("G3").src = "Images/Green_Square.png";
 						break;
 					case "B3":
-						document.getElementById("B3").src = "Blue_Square.png";
+						document.getElementById("B3").src = "Images/Blue_Square.png";
 						break;
    					case "R4": 
-						document.getElementById("R4").src = "Red_Square.png";
+						document.getElementById("R4").src = "Images/Red_Square.png";
 						break;
 					case "P4": 
-						document.getElementById("P4").src = "Purple_Square.png";
+						document.getElementById("P4").src = "Images/Purple_Square.png";
 						break;
 					case "G4":
-						document.getElementById("G4").src = "Green_Square.png";
+						document.getElementById("G4").src = "Images/Green_Square.png";
 						break;
 					case "B4":
-						document.getElementById("B4").src = "Blue_Square.png";
+						document.getElementById("B4").src = "Images/Blue_Square.png";
 						break;
    					case "R42": 
-						document.getElementById("R42").src = "Red_Square.png";
+						document.getElementById("R42").src = "Images/Red_Square.png";
 						break;
 					case "P5": 
-						document.getElementById("P5").src = "Purple_Square.png";
+						document.getElementById("P5").src = "Images/Purple_Square.png";
 						break;
 					case "G5":
-						document.getElementById("G5").src = "Green_Square.png";
+						document.getElementById("G5").src = "Images/Green_Square.png";
 						break;
 					case "P52": 
-						document.getElementById("P52").src = "Purple_Square.png";
+						document.getElementById("P52").src = "Images/Purple_Square.png";
 						break;
 					case "B5":
-						document.getElementById("B5").src = "Blue_Square.png";
+						document.getElementById("B5").src = "Images/Blue_Square.png";
 						break;
 					case "R5": 
-						document.getElementById("R5").src = "Red_Square.png";
+						document.getElementById("R5").src = "Images/Red_Square.png";
 						break;
         				alert('Square Unmarked');
        					break;
@@ -186,7 +188,6 @@
 					}
 				}
 			}
-        }
         function VoucherSourcetoPrint(source) {
             return "<html><head><script>function step1(){\n" +
                     "setTimeout('step2()', 10);}\n" +
@@ -194,29 +195,25 @@
                     "</scri" + "pt></head><body onload='step1()'>\n" +
                     "<img src='" + source + "' /></body></html>";
         }
-        function CardPrint() {
-           var divContents = document.getElementById("photo").innerHTML;
+        function VoucherPrint(source) {
             Pagelink = "about:blank";
             var pwa = window.open(Pagelink, "_new");
-            pwa.document.write('<html>');
-            pwa.document.write('<body > <h1>Div contents are <br>');
-            pwa.document.write(divContents);
-            pwa.document.write('</body></html>');
+            pwa.document.open();
+            pwa.document.write(VoucherSourcetoPrint(source));
             pwa.document.close();
-            pwa.print();
         }
-	    function ChangeTextIL(document){
+	    function ChangeTextIL(){
 		document.getElementById("divElement").innerHTML="Information Literacy: 1) Finding Scholarly Resources Effectively, 2) Fake News & Evaluating Information, 3) Plagiarism. Right-click to add Code from a completed workshop.";
 	}
-	    function ChangeTextDL(document){
+	    function ChangeTextDL(){
 		document.getElementById("divElement").innerHTML="Digital Literacy: 1) Your Wake Tech Accounts & Using Office 365 Apps, 2) Using NC Live & Library Databases. Right-click to add Code from a completed workshop.";
 	}
-	    function ChangeTextAD(document){
+	    function ChangeTextAD(){
 		document.getElementById("divElement").innerHTML="Adulting Life Skills: 1) Adulting 101, 2) Meal Planning 101, 3) Talking via Tech: Social Media Etiquette. Right-click to add Code from a completed workshop.";
 	}
-	    function ChangeTextCR(document){
+	    function ChangeTextCR(){
 		document.getElementById("divElement").innerHTML="Crafts: 1) Wish Bracelets, 2) Woven Bowls. Right-click to add Code from a completed workshop.";
 	}
-            function ChangeTextFR(document){
+            function ChangeTextFR(){
 		document.getElementById("divElement").innerHTML="This block does not need to be filled by a program or workshop.";
 	}
